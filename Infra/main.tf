0cdde7f42835fc1eecc039b23dc298bb7f6c24f2
Kush Patel <kushp011016@gmail.com>
2025-06-23 11:20:08 -0400

    Update main.tf to reference existing security group data source for EC2 instance

--- Infra/main.tf
+++ Infra/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "hello_ec2" {
   ami                         = "ami-0fa377108253bf620" 
   instance_type               = "t2.micro"
   key_name                    = "test"
-  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  vpc_security_group_ids      = [data.aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
   user_data = <<-EOF
